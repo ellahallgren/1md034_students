54b47870f32a82c022349e5c1290b1fe486841e8
ellahallgren <ellam_hallgren@hotmail.com>
2020-02-03 16:26:06 +0100

    4-3

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -1,10 +1,18 @@
-function menuItem(name, kCal, pic, gluten, lactose) {
+
+
+var menuItem = function(name, kCal, pic, gluten, lactose) {
     this.name = name;
     this.kCal = kCal;
     this.pic = pic;
     this.gluten = gluten;
     this.lactose = lactose;
-}
+};
+
+function calInfo(burger) {
+    
+    return "burger: " + burger.name + "\nkCal: " + burger.kCal;
+};
+
 
 let NastyBurger = new menuItem("Nasty Burger", 1300, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-h-DSEvwRwh77HkNEWyFZeINOd-6Pa__2vyxquayflbTmhRC-Iw&s", true, false);
 
@@ -12,6 +20,7 @@ let NiceBurger = new menuItem("Nice Burger", 2000, "https://encrypted-tbn0.gstat
 
 let NamnamBurger = new menuItem("Namnam Burger", 1500, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSx_MeDe_E_ww_fV62nDaOxtFk3P09xGSEUXFEf_lHsyzGBG86x&s", true, true);
 
-let NiceBurger = new menuItem("Next Burger", 2000, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8HComNXsGM-7olg6V6kOi8P9Y-YE24NM5tq4CDJOrgm4Uh-hR&s", false, false);
+let NextBurger = new menuItem("Next Burger", 2000, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8HComNXsGM-7olg6V6kOi8P9Y-YE24NM5tq4CDJOrgm4Uh-hR&s", false, false);
 
-let NamnamBurger = new menuItem("Normal Burger", 1400, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSx_MeDe_E_ww_fV62nDaOxtFk3P09xGSEUXFEf_lHsyzGBG86x&s", true, true);
+let NormalBurger = new menuItem("Normal Burger", 1400, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSx_MeDe_E_ww_fV62nDaOxtFk3P09xGSEUXFEf_lHsyzGBG86x&s", true, true);
+
