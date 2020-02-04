f4e392e224280d6d573d4727db1bc5371d750ce8
ellahallgren <ellam_hallgren@hotmail.com>
2020-02-04 21:37:10 +0100

    4-7

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -18,13 +18,44 @@ let NextBurger = new menuItem("Next Burger", 2000, "https://encrypted-tbn0.gstat
 
 let NormalBurger = new menuItem("Normal Burger", 1400, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSx_MeDe_E_ww_fV62nDaOxtFk3P09xGSEUXFEf_lHsyzGBG86x&s", true, true);
 
-document.getElementById("b1").innerHTML = NastyBurger.name;
+var menu = [NastyBurger, NiceBurger, NamnamBurger, NextBurger, NormalBurger];
+/*var ids = ["b1", "b2", "b3", "b4", "b5"];
+for (var x = 0; x < 5; x++)
+{
+	document.getElementById(ids[x]).innerHTML = menu[x].name;
+  if(menu[x].lactose == true) {
+  let id = document.getElementById(ids[x]);
+  let btn = document.createElement("p");
+  let txt = document.createTextNode("Contains lactose");
+  btn.appendChild(txt);
+  id.appendChild(btn);
+  }
+  if(menu[x].gluten == true) {
+  let id = document.getElementById(ids[x]);
+  let btn = document.createElement('p'); 
+  let txt = document.createTextNode("Contains gluten");
+  btn.appendChild(txt);
+  id.appendChild(btn);
+  }
+
+}*/
+
+
+/*document.getElementById("b1").innerHTML = NastyBurger.name;
 document.getElementById("b2").innerHTML = NiceBurger.name;
 document.getElementById("b3").innerHTML = NamnamBurger.name;
 document.getElementById("b4").innerHTML = NextBurger.name;
 document.getElementById("b5").innerHTML = NormalBurger.name;
-
+*/
 function calInfo(burger) {
     
     return "burger: " + burger.name + "\nkCal: " + burger.kCal;
 };
+
+const mat = new Vue({
+el: '#myID',
+data: {
+menu: menu
+},
+
+})
--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,11 +1,35 @@
-const vm = new Vue({
-  el: '#myID',
-  data: {
-    b1: NastyBurger.name,
-    b2: NiceBurger.name,
-    b3: NamnamBurger.name,
-    b4: NextBurger.name,
-    b5: NormalBurger.name
-  }
-})
+<div id="myID">
+<div v-for = "(x,index) in menu">
+<h4>
+{{ x.name }}
+</h4>
+<ul>
+<li> {{ x.kCal }} </li>
+</ul>
+</div>
+</div>   
 
+
+  
+   /* <div>
+    <script src="https://vuejs.org/js/vue.js"></script>
+    <div id="myID">
+       {{ b1 }}<br>
+        {{ b2 }}<br>
+        {{ b3 }}<br>
+        {{ b4 }}<br>
+        {{ b5 }}
+    <div> 
+    <p id="b1"> 
+    </p>
+    <p id="b2">
+    </p>
+    <p id="b3">
+    </p> 
+    <p id="b4">  
+    </p>
+    <p id="b5">
+    </p>
+    </div>        
+    </div>
+    </div>*/
